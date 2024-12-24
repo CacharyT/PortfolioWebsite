474144d35b700b8bf881b54de964d1215a52d1f1
CacharyT <tolenti2@go.stockton.edu>
2024-12-24 15:36:14 -0800

    Added web analytics

--- portfolio-website/app/layout.tsx
+++ portfolio-website/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Navbar } from './components/nav'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -46,6 +47,7 @@ export default function RootLayout({
             </div>
           {children}
           <SpeedInsights />
+          <Analytics />
         </main>
       </body>
     </html>
